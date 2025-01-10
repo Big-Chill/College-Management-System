4176e1dd85f2a894b1da25edc326d2afa493b444
Rohit Nandy <rohitnandy39@gmail.com>
2025-01-10 22:48:01 +0530

    Added service dependecy container and updated the course router

--- containers/app_container.py
+++ containers/app_container.py
@@ -1,9 +1,11 @@
 from dependency_injector import containers, providers
 from .database_container import DatabaseContainer
 from .message_broker_container import MessageBrokerContainer
+from .service_container import ServiceContainer
 import configuration
 
 class AppContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
     database_container = providers.Container(DatabaseContainer)
     message_broker_container = providers.Container(MessageBrokerContainer)
+    service_container = providers.Container(ServiceContainer)
---
+++ containers/service_container.py
@@ -0,0 +1,12 @@
+from dependency_injector import containers, providers
+from repositories import CassandraRepository, RedisRepository, KafkaRepository
+from services import CourseService
+import configuration
+
+class ServiceContainer(containers.DeclarativeContainer):
+    config = providers.Configuration()
+    config.from_dict(configuration.configuration)
+    cassandra_repository = providers.Singleton(CassandraRepository, contact_points=config.CASSANDRA_CONTACT_POINTS, keyspace=config.CASSANDRA_KEYSPACE)
+    redis_repository = providers.Singleton(RedisRepository, host=config.REDIS_HOST, port=config.REDIS_PORT, db=0)
+    kafka_repository = providers.Singleton(KafkaRepository, bootstrap_servers=config.KAFKA_HOST)
+    course_service = providers.Factory(CourseService, db_repository=cassandra_repository, cache_repository=redis_repository, search_repository=cassandra_repository, message_broker_repository=kafka_repository)
